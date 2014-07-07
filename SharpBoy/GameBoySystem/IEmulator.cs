d09e7c37915acdb69f359f0051f8e128d0e27c6c
Bryan Davis <reactiontime@users.noreply.github.com>
2014-07-06 21:35:46 -0700

    All things must begin somewhere

---
+++ SharpBoy/GameBoySystem/IEmulator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace SharpBoy.GameBoySystem
+{
+    public delegate void RenderEventHandler(byte[] data);
+
+    interface IEmulator
+    {        
+        event RenderEventHandler RenderHandler;
+
+        string CurrentGame { get; }
+        int RenderHeight { get; }
+        int RenderWidth { get; }
+        bool Stop { get; set; }
+
+        void Run();
+        void LoadRom(string filename);
+        void KeyUp(Key key);
+        void KeyDown(Key key);
+        void LoadState(string saveStateFile);
+        void SaveState();
+    }
+}
---
+++ SharpBoy/GameBoySystem/ViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace SharpBoy.GameBoySystem
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        } 
+    }
+}
